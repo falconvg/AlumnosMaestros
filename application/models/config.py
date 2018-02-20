533eff57d9f54304c3af6fade70a673593a3158f
falconvg <falconvg@hotmail.com>
2018-02-19 20:23:13 -0600

    Update config.py

--- application/models/config.py
+++ application/models/config.py
@@ -1,6 +1,6 @@
 import web
 
-db_host = 'jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	'
+db_host = 'jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
 db_name = 'gkikf0shbqq8jonf'
 db_user = 'e9otwnhxe2p20tbn'
 db_pw = 'yf2sy2f06iq450eu'
